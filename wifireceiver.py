5b78bed9a3cb07312fafad2c61c554f4ef763ca7
Alejandro Ruiz <dacoyal@alejandros-mbp.lan>
2022-01-25 11:29:57 -0500

    first commit

---
+++ wifireceiver.py
@@ -0,0 +1,103 @@
+import numpy as np
+import sys
+import commpy as comm
+import commpy.channelcoding.convcode as check
+
+#some testing arrays
+hello_array = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+count9_array = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+abcde_array = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+'''
+    begin_zero_padding = Number of zeros appended at the beginning of the message
+    in Level 4
+
+    message = The message used to create WiFi packet
+    
+    length = the length of the message
+'''
+
+
+def WifiReceiver(*args):
+    output = args[0]
+    level = int(args[1])
+    length = 0
+    message = ""
+    begin_zero_padding = 0
+
+    '''DELETE THE OUTPUT = PART! ITS JUST FOR TESTING NOW'''
+    output = abcde_array
+    nfft = 64
+    '''DELETE THE OUTPUT = PART! ITS JUST FOR TESTING NOW'''
+    
+    #Preamble Detection
+    if level >= 4:
+        pass
+
+    #OFDM Demod
+    if level >= 3:
+        pass
+    
+    #Viterbi Soft Decoding
+    if level >= 2:
+        pass
+
+    #De-interleaving
+    if level >= 1:
+        '''
+        Intially the bits are going to be padding of 0s.
+        len_binary :128 bits
+            first last 14 bits of len_binary represent the length of the message
+            bits: [113, 127]
+        '''
+        Interleave = np.reshape(np.transpose(np.reshape(np.arange(1, 2*nfft+1, 1),[-1,4])),[-1,])
+        start_length_bit = 113
+        n = len(output)
+        n_row = 128
+        bits = np.zeros(n - 128)
+
+        '''get length of message'''
+        for i in range(start_length_bit, n_row):
+            bit = output[i]
+            if bit == 1:
+                binary_index = n_row - (i + 1)
+                length += 2**(binary_index)
+
+        '''get the message'''
+        encoded_message = output[n_row:]
+        n_encoded = len(encoded_message)
+        n_interleave = len(Interleave)
+
+        for row in range(0, n_encoded, n_row):
+            output_row = encoded_message[row:row+n_row]
+            print(output_row)
+            for i in range(n_interleave):
+                col = (Interleave - 1)[i]
+                bits[n_row*row + col] = output_row[row*n_row + i]
+
+
+        '''decode the message'''
+        n_bits = len(bits)
+        bits_per_num = 8
+        ascii_values = np.zeros((n_bits//bits_per_num,), dtype = int)
+
+        #build the ASCII values of each number
+        for start in range(0, n_bits, bits_per_num):
+            num = 0
+            for i in range(bits_per_num):
+                bit = bits[start + i]
+                if (bit == 1):
+                    exp =  (bits_per_num - 1) - (i % bits_per_num)
+                    num += int(2**(exp))
+
+            ascii_values[start//bits_per_num] = num
+
+        #convert the ASCII values to letters
+        for num in ascii_values:
+            message += chr(num)
+
+    print(message + ",", length)
+    return begin_zero_padding, message, length
+
+if __name__ == '__main__':
+    WifiReceiver(sys.argv[1], sys.argv[2])
